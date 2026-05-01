fa76910740d8e1884986593ba2c0d1e861411256
Mayra <mm975pa@gmail.com>
2026-05-01 16:17:20 -0600

    Add project and team information to servidor_control.py
    Added project information and team members to the server control file.

--- Servidor_Python/servidor_control.py
+++ Servidor_Python/servidor_control.py
@@ -1,1 +1,5 @@
-
+"""
+OBJETIVO: (Ej: Control de sensores y comunicación MQTT)
+INTEGRANTES: (Mayra Paola Martinez Aranda - Código, Nissi Sarahi Prats Ramirez - Código, Erik Fabian Gonsalez Jimenez - Código)
+PROYECTO: BroccoSort AI
+"""
