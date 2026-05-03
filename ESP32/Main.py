02c10cb2b75a943808091e067dc9aa94b3ca28d3
Mayra <mm975pa@gmail.com>
2026-05-02 23:03:42 -0600

    Refine comments and formatting in Main.py
    Updated comments and formatting in Main.py for clarity.

--- ESP32/Main.py
+++ ESP32/Main.py
@@ -1,16 +1,18 @@
-# OBJETIVO:
+"""
+OBJETIVO:
 Implementar un sistema automatizado de clasificación de hortalizas que utiliza visión artificial y sensores físicos para categorizar brócoli según su estado de madurez (color) y dimensiones (tamaño). El sistema busca optimizar la producción agrícola local mediante una banda transportadora controlada por un ESP32 S3 CAM, la cual desvía automáticamente el producto de baja calidad, emite alertas sonoras y visuales, y permite el monitoreo remoto de estadísticas de producción a través de una interfaz web y notificaciones en la nube.
 
-# INTEGRANTES:
+INTEGRANTES:
 
-Mayra Paola Martínez Aranda(22240233) (Código)
+Mayra Paola Martínez Aranda (Código)
 
-Nissi Sarahi Prats Ramírez(23240003) (Código)
+Nissi Sarahi Prats Ramírez (Código)
 
-Erik Fabian Gonsalez Jimenez(23240022) (Código)
+Erik Fabian Gonsalez Jimenez (Código)
 
-# PROYECTO:
+PROYECTO:
 "BroccoSort AI: Sistema Automatizado de Clasificación de Hortalizas por Visión y Morfología"
+"""
 
 from dispositivos import SensorBox, ActuatorBox
 import time
