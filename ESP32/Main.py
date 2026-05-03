a9fc445863ae89ead3cc7a622f5cfd73bb5754d5
Mayra <mm975pa@gmail.com>
2026-05-02 23:48:49 -0600

    Refactor project objective description for clarity

--- ESP32/Main.py
+++ ESP32/Main.py
@@ -1,6 +1,11 @@
 """
 OBJETIVO:
-Implementar un sistema automatizado de clasificación de hortalizas que utiliza visión artificial y sensores físicos para categorizar brócoli según su estado de madurez (color) y dimensiones (tamaño). El sistema busca optimizar la producción agrícola local mediante una banda transportadora controlada por un ESP32 S3 CAM, la cual desvía automáticamente el producto de baja calidad, emite alertas sonoras y visuales, y permite el monitoreo remoto de estadísticas de producción a través de una interfaz web y notificaciones en la nube.
+Implementar un sistema automatizado de clasificación de hortalizas que utiliza visión artificial 
+y sensores físicos para categorizar brócoli según su estado de madurez (color) y dimensiones (tamaño). 
+El sistema busca optimizar la producción agrícola local mediante una banda transportadora 
+controlada por un ESP32 S3 CAM, la cual desvía automáticamente el producto de baja calidad, 
+emite alertas sonoras y visuales, y permite el monitoreo remoto de estadísticas de producción 
+a través de una interfaz web y notificaciones en la nube.
 
 INTEGRANTES:
 - Mayra Paola Martínez Aranda(22240233) 
