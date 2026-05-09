fa54ba5b546108df93930b075630bba705c5a417
Mayra <mm975pa@gmail.com>
2026-05-08 20:54:38 -0600

    Revise project objective and team member information
    Updated project objective and team member details in Main.py.

--- ESP32/Main.py
+++ ESP32/Main.py
@@ -1,14 +1,13 @@
 """
-OBJETIVO:
-Coordinar el sistema de clasificación mediante la integración de la HAL y el protocolo MQTT. 
-Publica telemetría constante y reacciona a comandos remotos para la clasificación de hortalizas.
+# OBJETIVO:
+Implementar un sistema automatizado de clasificación de hortalizas que utiliza visión artificial y sensores físicos para categorizar brócoli según su estado de madurez (color) y dimensiones (tamaño). El sistema busca optimizar la producción agrícola local mediante una banda transportadora controlada por un ESP32 S3 CAM, la cual desvía automáticamente el producto de baja calidad, emite alertas sonoras y visuales, y permite el monitoreo remoto de estadísticas de producción a través de una interfaz web y notificaciones en la nube.
 
-INTEGRANTES:
-- Mayra Paola Martínez Aranda (22240233) 
-- Nissi Sarahi Prats Ramírez (23240003) 
-- Erik Fabian Gonsalez Jimenez (23240022)  
+# INTEGRANTES:
+- Mayra Paola Martínez Aranda(22240233) 
+- Nissi Sarahi Prats Ramírez(23240003) 
+- Erik Fabian Gonsalez Jimenez(23240022)  
 
-PROYECTO:
+# PROYECTO:
 "BroccoSort AI: Sistema Automatizado de Clasificación de Hortalizas por Visión y Morfología"
 """
 
