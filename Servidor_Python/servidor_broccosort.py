e59e3a803972ca48c9eff1844d452c7394c304d2
Mayra <mm975pa@gmail.com>
2026-05-21 21:31:07 -0600

    Update project description and objectives in servidor_broccosort.py

--- Servidor_Python/servidor_broccosort.py
+++ Servidor_Python/servidor_broccosort.py
@@ -1,19 +1,12 @@
 """
-OBJETIVO:
-Implementar un sistema automatizado de clasificación de hortalizas que utiliza visión artificial 
-y sensores físicos para categorizar brócoli según su estado de madurez (color) y dimensiones (tamaño). 
-El sistema busca optimizar la producción agrícola local mediante una banda transportadora 
-controlada por un ESP32 S3 CAM, la cual desvía automáticamente el producto de baja calidad, 
-emite alertas sonoras y visuales, y permite el monitoreo remoto de estadísticas de producción 
-a través de una interfaz web y notificaciones en la nube.
-
-INTEGRANTES:
-- Mayra Paola Martínez Aranda(22240233) 
-- Nissi Sarahi Prats Ramírez(23240003) 
-- Erik Fabian Gonsalez Jimenez(23240022)  
-
-PROYECTO:
-"BroccoSort AI: Sistema Automatizado de Clasificación de Hortalizas por Visión y Morfología"
+NOMBRE DEL PROYECTO: BroccoSort AI: Sistema Automatizado de Clasificación
+de Hortalizas por Visión y Morfología
+OBJETIVO: Integración de IA para Captura remota y transmisión de imágenes
+de hortalizas hacia el servidor central de IA.
+INTEGRANTES: 
+- Mayra Paola Martínez Aranda (22240233)
+- Nissi Sarahi Prats Ramírez (23240003)
+- Erik Fabian Gonsalez Jimenez (23240022)
 """
 
 import paho.mqtt.client as mqtt
